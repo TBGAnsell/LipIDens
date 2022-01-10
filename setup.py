ef253ba2c24883fdd0535d38b3ebea5d4c0ebe10
TBGAnsell <58514791+TBGAnsell@users.noreply.github.com>
2022-01-10 11:04:58 +0000

    setup file commit

---
+++ setup.py
@@ -0,0 +1,67 @@
+"""
+LipIDens
+
+A protocol for simulation assissted interpretation of lipid-like densities.
+
+"""
+
+from setuptools import setup, find_packages
+import re
+
+with open("README.md", "r") as fle:
+    ld=fle.read()
+
+try:
+    with open("lipidens/_version.py", "rt") as v_fle:
+        v_str=v_fle.read()
+        v_s=r"^__version__ = ['\"]([^'\"]*)['\"]"
+        v=re.search(v_s, v_str, re.M)
+        curr_ver=v.group(1)
+except Exception as e:
+    raise RuntimeError("Unable to locate version string:", e)
+
+
+setup(
+  name = 'lipidens',
+  version = curr_ver,
+  license="MIT",
+  author = 'T. Bertie Ansell, Wanling Song',
+  author_email = 'bertie.ansell@bioch.ox.ac.uk',
+  url = 'https://github.com/TBGAnsell/LipIDens',
+  description="A protocol for simulation assisted interpretation of lipid-like densities.",
+  long_description=ld,
+  long_description_content_type="text/markdown",
+  packages=find_packages(),
+  include_package_data=True,
+  keywords = ['simulation', 'lipid', 'density', 'binding site'],
+  python_requires='>=3.6, <4, !=3.8*',
+  install_requires=['kneebow',
+      'logomaker',
+      'matplotlib>=3.3.4',
+      'mdtraj',
+      'networkx',
+      'numpy',
+      'p-tqdm',
+      'pandas',
+      'pylipid>=1.5.14',
+      'python-louvain',
+      'scikit-learn',
+      'scipy',
+      'seaborn',
+      'statsmodels',
+      'tqdm',
+      'vermouth>=0.7.2',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    'Topic :: Scientific/Engineering :: Chemistry',
+    'Topic :: Scientific/Engineering :: Medical Science Apps.',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
