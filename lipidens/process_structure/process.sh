f84a9a3cbca9c90f13bfa2cdd1813a8f2008d31c
Bertie Ansell <bertie.ansell@wadham.ox.ac.uk>
2023-01-22 18:15:47 +0000

    Processing AT file improved. Update requirements networkx version.

--- lipidens/process_structure/process.sh
+++ lipidens/process_structure/process.sh
@@ -6,9 +6,29 @@
 #1 - protein_AT_full
 #2 - protein_rotate
 
+
 if [ "${1#*.}" = "pdb" ]; then input="${1%%.*}"; else echo "Check input file is in pdb format"; fi
 
-gmx editconf -f ${input}.pdb -o ${input}_princ.pdb -princ -rotate ${2} -center 0 0 0 >& gmx_tmp<<EOF
+pdb2pqr30 --ff CHARMM --keep-chain ${input}.pdb prot_pqr.pdb >& gmx_tmp
+
+gmx pdb2gmx -f prot_pqr.pdb -ignh -ff charmm27 -water tip3p -o prot_sol.pdb >& gmx_tmp
+
+gmx editconf -f prot_sol.pdb -o ${input}_princ.pdb -princ -d 8 -c -rotate ${2} >& gmx_tmp<<EOF
+0
+EOF
+
+cat << EOF > em_short.mdp
+integrator = steep
+nsteps = 5000
+emtol = 100
+emstep = 0.001
+EOF
+
+gmx grompp -f em_short.mdp -c ${input}_princ.pdb -r ${input}_princ.pdb -o em.tpr -maxwarn 5 >& gmx_tmp
+
+gmx mdrun -deffnm em -c prot_at_em_princ.pdb >& gmx_tmp
+
+gmx trjconv -f prot_at_em_princ.pdb -o ${input}_princ.pdb -s em.tpr >& gmx_tmp<<EOF
 0
 EOF
 
